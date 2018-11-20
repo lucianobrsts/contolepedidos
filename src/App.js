c3b43cc8993045b1afd556b1b74b106841ba462a
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-11-19 23:40:41 -0300

    Adicionado BootStrap na aplicação.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Teste React/Redux
+            Teste React/Redux + BootStrap
           </a>
         </header>
       </div>
