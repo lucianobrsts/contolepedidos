648df10391008a63da59433800615e9f8224af8e
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-11-21 19:46:13 -0300

    Criado formulário de cadastro dos Pedidos.

--- src/container/Cadastro.js
+++ src/container/Cadastro.js
@@ -1,9 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Cadastro extends Component {
     render() {
-        return(
-            <h1>Cadastro de Pedidos</h1>
+        return (
+
+            <form>
+                <div className="form-group">
+                    <input type="nome" className="form-control" id="nomeCliente" aria-describedby="nomeHelp" placeholder="Nome do Cliente" />
+                </div>
+                <div className="form-group">
+                    <input type="cpf" className="form-control" id="cpf" placeholder="CPF" />
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" id="descricaoPedido" rows="3" placeholder="Descrição do Pedido"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Registrar...</button>
+            </form>
+
         )
     }
 }
