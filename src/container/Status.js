ca129a505fc077591182d1d3e156e5e746a0fa13
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-11-22 01:26:27 -0300

    Criado componente status.

--- src/container/Status.js
+++ src/container/Status.js
@@ -1,9 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Legenda from '../ui/Legenda';
+import seta from '../img/setas.png';
 
 export default class Status extends Component {
     render() {
-        return(
-            <h1>Status do Pedido</h1>
-        )
+        return (
+            <div>
+                <br />
+                <h2>Avançar Status do Pedido</h2>
+                <br />
+
+                <table class="table table-bordeless table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col" style={{ width: '60%' }}>Cliente</th>
+                            <th scope="col" style={{ width: '20%', textAlign: 'center' }}>Status</th>
+                            <th scope="col" style={{ width: '20%', textAlign: 'center' }}>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Luciano Brito</th>
+                            <td style={{ textAlign: 'center' }}>Cor pedido</td>
+                            <td style={{ textAlign: 'center' }}><img src={seta} style={{ width: '30px' }} /></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <Legenda />
+
+            </div >
+        );
     }
 }
