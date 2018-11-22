ed8776b3c1df11d637b869f1c9bce19d857c53c4
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-11-22 01:20:51 -0300

    Criado ui Legenda.

---
+++ src/ui/Legenda.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import vermelho from '../img/bolaVermelha.png';
+import amarelo from '../img/bolaAmarela.png';
+import verde from '../img/bolaVerde.png';
+import azul from '../img/bolaAzul.png';
+
+export default class Legenda extends Component {
+    render() {
+        return (
+            <div>
+                <p> ATENÇÃO: Dirija-se ao caixa quando seu pedido estiver azul. </p>
+                <p>Legenda</p>
+                <img src={vermelho} style={{ width: '30px' }} /> <span>Pedido Novo</span> <br />
+                <img src={amarelo} style={{ width: '30px' }} /> <span>Pedido sendo preparado</span> <br />
+                <img src={verde} style={{ width: '30px' }} /> <span>Pedido em conferência</span> <br />
+                <img src={azul} style={{ width: '30px' }} /> <span>Pronto para pagamento</span> <br />
+            </div>
+        );
+    }
+}
