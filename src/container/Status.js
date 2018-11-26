9b973a41bb1049558e93ea93e5dca79b7aaaee81
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-11-25 23:05:30 -0300

    Modificado arquivo db.json.

--- src/container/Status.js
+++ src/container/Status.js
@@ -9,15 +9,69 @@ import bolaVermelha from '../img/bolaVermelha.png';
 import bolaAmarela from '../img/bolaAmarela.png';
 import bolaVerde from '../img/bolaVerde.png';
 import bolaAzul from '../img/bolaAzul.png';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/pedidos';
 
 export default class Status extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props)
         this.state = {
+            exibirPedido: false,
             pedidos: [{
-
+                nome: '',
+                descricao: '',
+                cpf: '',
+                status: 'Novo',
+                hora: new Date(),
+                id: 0
             }]
         }
+        this._atualizarPedido = this._atualizarPedido.bind(this);
+        this._excluirPedido = this._excluirPedido.bind(this);
+        this._acompanharPedido = this._acompanharPedido.bind(this);
+        this._buscarPedidos = this._buscarPedidos.bind(this);
+    }
+
+    _atualizarPedido(pedido) {
+        console.log(pedido.status);
+        var status = pedido.status;
+        if (pedido.status === 'Novo') {
+            status = 'Pedido sendo preparo';
+        } else if (pedido.status === 'Pedido sendo preparo') {
+            status = 'Pedido em conferência';
+        } else if (pedido.status === 'Pedido em conferência') {
+            status = 'Pronto para pagamento';
+        } else if (pedido.status === 'Pronto para pagamento') {
+            status = pedido.status;
+        }
+        var p = {
+            "nome": pedido.nome,
+            "descricao": pedido.descricao,
+            "cpf": pedido.cpf,
+            "status": status,
+            "dataHora": pedido.dataHora,
+            "id": pedido.id
+        };
+        this.props.atualizarPedido(p);
+        this._buscarPedidos();
+    }
+
+    _handleClick = (event) => {
+        event.preventDefault()
+        this.setState({
+            exibirPedido: !this.state.exibirPedido
+        })
+    }
+
+
+    _excluirPedido(pedido) {
+        this.props.excluirPedido(pedido);
+        this._buscarPedidos();
+    }
+
+    _buscarPedidos() {
+        //this.props.buscarPedidos();
     }
 
     _corStatus(pedido) {
@@ -34,6 +88,7 @@ export default class Status extends Component {
         return cor;
     }
     render() {
+        const { pedidos, carregando } = this.state
         return (
             <div>
                 <br />
@@ -49,15 +104,15 @@ export default class Status extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {dbjson.pedidos.map((pedido, index) => {
+                        {pedidos.map((pedido) => {
                             return (
                                 < tr key={pedido.id}>
                                     <th scope="row" style={{ fontSize: '20px' }}>{pedido.nome}</th>
                                     <td style={{ textAlign: 'center' }}>{this._corStatus(pedido)}</td>
                                     <td style={{ textAlign: 'center' }}>
                                         <input style={{ paddingRight: '10px' }} name="carregarPedido" type="image" id="carregarPedido" src={carregarPedido} alt="Carregar Pedido" data-target="#exampleModal" data-toggle="modal"></input>
-                                        <input style={{ paddingRight: '10px' }} name="avançarStatus" type="image" id="avançarStatus" src={avancarStatus} alt="Avançar Status"></input>
-                                        <input style={{ paddingRight: '10px' }} name="excluirPedido" type="image" id="excluirPedido" src={excluirPedido} alt="Excluir Pedido"></input>
+                                        <input style={{ paddingRight: '10px' }} name="avançarStatus" type="image" id="avançarStatus" src={avancarStatus} alt="Avançar Status" onClick={(e) => this._atualizarPedido(pedido, e)}></input>
+                                        <input style={{ paddingRight: '10px' }} name="excluirPedido" type="image" id="excluirPedido" src={excluirPedido} alt="Excluir Pedido" onClick={(e) => this._excluirPedido(pedido, e)}></input>
                                     </td>
                                 </tr>
                             )
